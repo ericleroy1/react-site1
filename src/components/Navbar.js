3743b94fb2c77936320dc420dc13d85322f83714
ericleroy1 <54584831+ericleroy1@users.noreply.github.com>
2021-02-25 11:59:04 +1100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from './Button';
+import './Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMenu = () => setClick(false);
+
+    const [button, setButton] = useState(true);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                        TRAVEL <i className='fab fa-typo3' />
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/membership_plans' className='nav-links' onClick={closeMenu}>
+                                Membership Plans
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/faqs' className='nav-links' onClick={closeMenu}>
+                                FAQs
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign_up' className='nav-links' onClick={closeMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar
